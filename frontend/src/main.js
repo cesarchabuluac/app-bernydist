5b973785d02cbbaaadc223df30ad7572bd45e22e
Cesar chab <cesarchabuluac@gmail.com>
2021-12-09 17:05:37 -0600

    Fix main

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,13 +3,26 @@ import App from "./App.vue";
 import router from "./router";
 import * as VueGoogleMaps from "vue2-google-maps";
 
+
+//Sweet alert
+import VueSweetalert2 from 'vue-sweetalert2';
+
+// If you don't need the styles, do not connect
+import 'sweetalert2/dist/sweetalert2.min.css';
+
+//Mixins
+import http from "./mixins/http";
+
 Vue.config.productionTip = false;
 
 Vue.use(VueGoogleMaps, {
-  load: {
-    key: "AIzaSyC0lnIcO0oeG0hMl0hgG_DK_xfNRCUGwpA",
-  },
+	load: {
+		key: "AIzaSyC0lnIcO0oeG0hMl0hgG_DK_xfNRCUGwpA"
+	}
 });
+
+Vue.use(VueSweetalert2);
+Vue.mixin(http);
 
 new Vue({
 	router,
